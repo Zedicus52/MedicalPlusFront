b8a77bb4fb67d16d75e09454c36d23b24a563315
Давуд Рамазанов <95874233+Davud1Ramazanov@users.noreply.github.com>
2024-01-25 22:58:19 +0200

    A user search component has been created

---
+++ ViewModel/FindUserVM.cs
@@ -0,0 +1,77 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalPlusFront.ViewModel
+{
+    class FindUserVM : BaseVM
+    {
+        public string PhoneNumberInput
+        {
+            get => _phoneNumberInput;
+            set
+            {
+                _phoneNumberInput = value;
+                OnPropertyChanged("PhoneNumberInput");
+            }
+        }
+
+        public string IdNumberInput
+        {
+            get => _idNumberInput;
+            set
+            {
+                _idNumberInput = value;
+                OnPropertyChanged("IdNumberInput");
+            }
+        }
+
+        public string FullNameInput
+        {
+            get => _fullNameInput;
+            set
+            {
+                _fullNameInput = value;
+                OnPropertyChanged("FullNameInput");
+            }
+        }
+
+        public string AfterDateInput
+        {
+            get => _afterDateInput;
+            set
+            {
+                _afterDateInput = value;
+                OnPropertyChanged("AfterDateInput");
+            }
+        }
+
+        public string BeforeDateInput
+        {
+            get => _beforeDateInput;
+            set
+            {
+                _beforeDateInput = value;
+                OnPropertyChanged("BeforeDateInput");
+            }
+        }
+
+        private string _phoneNumberInput;
+        private string _idNumberInput;
+        private string _fullNameInput;
+        private string _afterDateInput;
+        private string _beforeDateInput;
+
+        public FindUserVM()
+        {
+            PhoneNumberInput = string.Empty;
+            IdNumberInput = string.Empty;
+            FullNameInput = string.Empty;
+            AfterDateInput = string.Empty;
+            BeforeDateInput = string.Empty;
+        }
+    }
+}
