4e8d73148907d6614d69fdff0f9bce924a416580
Давуд Рамазанов <95874233+Davud1Ramazanov@users.noreply.github.com>
2024-01-27 00:52:30 +0200

    Added user creation component

---
+++ View/CreateUserCard.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedicalPlusFront.View
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateUserCard.xaml
+    /// </summary>
+    public partial class CreateUserCard : UserControl
+    {
+        public static readonly DependencyProperty IdCreateProperty =
+            DependencyProperty.Register("TextIdCreate", typeof(string), typeof(CreateUserCard));
+
+        public string TextIdCreate
+        {
+            get { return (string)GetValue(IdCreateProperty); } 
+            set { SetValue(IdCreateProperty, value);}
+        }
+
+        public CreateUserCard()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
---
+++ ViewModel/CreateUserVM.cs
@@ -0,0 +1,86 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalPlusFront.ViewModel
+{
+    class CreateUserVM : BaseVM
+    {
+        public string SurnameInput
+        {
+            get => _surnameInput;
+            set
+            {
+                _surnameInput = value;
+                OnPropertyChanged("SurnameInput");
+            }
+        }
+
+        public string NameInput
+        {
+            get => _nameInput;
+            set
+            {
+                _nameInput = value;
+                OnPropertyChanged("NameInput");
+            }
+        }
+
+        public string PatronymicInput
+        {
+            get => _surnameInput;
+            set
+            {
+                _patronymicInput = value;
+                OnPropertyChanged("PatronymicInput");
+            }
+        }
+
+        public string PhoneFaxInput
+        {
+            get => _phoneFaxInput;
+            set
+            {
+                _phoneFaxInput = value;
+                OnPropertyChanged("PhoneFaxInput");
+            }
+        }
+
+        public string BirthDateInput
+        {
+            get => _birthDateInput;
+            set
+            {
+                _birthDateInput = value;
+                OnPropertyChanged("BirthDateInput");
+            }
+        }
+
+        public RelayCommand CreateUserCommand
+        {
+            get
+            {
+                return _createUserCommand ?? (_createUserCommand = new RelayCommand(() =>
+                {
+                    SurnameInput = string.Empty;
+                    NameInput = string.Empty;
+                    PatronymicInput = string.Empty;
+                    PhoneFaxInput = string.Empty;
+                    BirthDateInput = string.Empty;
+                }));
+            }
+        }
+
+
+        public string _surnameInput;
+        public string _nameInput;
+        public string _patronymicInput;
+        public string _phoneFaxInput;
+        public string _birthDateInput;
+
+        public RelayCommand _createUserCommand;
+    }
+}
