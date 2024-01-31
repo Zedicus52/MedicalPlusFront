9201a31702ce75fa6ac0e0946b60cb5e8fff3e17
Давуд Рамазанов <95874233+Davud1Ramazanov@users.noreply.github.com>
2024-01-31 20:55:24 +0200

    A component with a list of users/doctors has been added

---
+++ ViewModel/ListOfPeopleVM.cs
@@ -0,0 +1,38 @@
+﻿using MedicalPlusFront.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalPlusFront.ViewModel
+{
+    internal class ListOfPeopleVM : BaseVM
+    {
+        public ObservableCollection<UserCard> ListOfPeople
+        {
+            get => _listPeople;
+            set
+            {
+                _listPeople = value;
+                OnPropertyChanged("ListOfPeople");
+            }
+        }
+
+        private ObservableCollection<UserCard> _listPeople;
+
+        public ListOfPeopleVM()
+        {
+            _listPeople = new ObservableCollection<UserCard>
+            {
+                new UserCard{ UserImg="/View/Icons/man.png", PatientName="Іванов Іван Іванович", PatientBirth="20.10.2014", PatientId="ID: 423"},
+                new UserCard{ UserImg="/View/Icons/man.png", PatientName="Іванов Іван Іванович", PatientBirth="20.10.2014", PatientId="ID: 231"},
+                new UserCard{ UserImg="/View/Icons/man.png", PatientName="Іванов Іван Іванович", PatientBirth="20.10.2014", PatientId="ID: 534"},
+                new UserCard{ UserImg="/View/Icons/man.png", PatientName="Іванов Іван Іванович", PatientBirth="20.10.2014", PatientId="ID: 123"},
+                new UserCard{ UserImg="/View/Icons/man.png", PatientName="Іванов Іван Іванович", PatientBirth="20.10.2014", PatientId="ID: 111"},
+                new UserCard{ UserImg="/View/Icons/man.png", PatientName="Іванов Іван Іванович", PatientBirth="20.10.2014", PatientId="ID: 333"}
+            };
+        }
+    }
+}
